b0c3c83ac9cfa5c93d4a3c53052fd20f7df9c8fb
Frank <frank@Franks-iMac.local>
2023-11-20 10:24:32 +0800

    feature: support product

--- config.go
+++ config.go
@@ -3,15 +3,35 @@ package nacos
 import (
 	"fmt"
 	"github.com/spf13/viper"
+	"os"
 )
 
 const (
-	publicNamespace = "public"
-	authConf        = "auth"
+	namespaceKey     = "NACOS_NAMESPACE"
+	defaultNamespace = "public"
+
+	// 定义dataId
+	// 权限验证
+	authConf = "auth"
+	// 产品服务
+	productConf = "product"
 )
 
+func GetNamespace() string {
+	ns := os.Getenv(namespaceKey)
+	if ns != "" {
+		return ns
+	}
+	return defaultNamespace
+}
+
 func GetAuthConfig() *viper.Viper {
-	return GetConfig(publicNamespace, authConf)
+	return GetConfig(GetNamespace(), authConf)
+}
+
+// GetProductConfig 获取产品配置
+func GetProductConfig() *viper.Viper {
+	return GetConfig(GetNamespace(), productConf)
 }
 
 func CheckGetAuthConfig() string {
